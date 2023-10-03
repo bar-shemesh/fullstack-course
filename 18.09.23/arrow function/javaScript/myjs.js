d99f22752e04d3f1eaf19bac28f9d3f038077151
unknown <barsh323@gmail.com>
2023-10-03 18:25:36 +0300

    14.9

--- 18.09.23/arrow function/javaScript/myjs.js
+++
@@ -1,8 +0,0 @@
-function invoke(){
-    func1(function(){console.log("hello"); });
-}
-func2(function(a, b, c) {console.log(`sum is ${a+b+c}`)});
-func2((a,b,c)=> console.log(`sum is${a+b+c}`));
-
-func3(function(height) { console.log(`height is ${height}`); })
-    
--- 28.09/תרגיל/javaScript/main.js
+++
@@ -1,7 +0,0 @@
-const displayLocation = () => {
-    getUserLocation((location) => console.log(`${location.latitude},${location.longitude}`));
-}
-
-const getUserLocation = (callback) =>{
-    navigator.geolocation.getCurrentPosition(position => callback(position.coords));
-}
